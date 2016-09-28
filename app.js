6340bb6e64aa16acb8ecae5f716706d3dcfe9613
runnerguy55 <michaelhoffmanj@gmail.com>
2016-09-28 15:10:40 -0600

    mobile improvements

--- app.js
+++ app.js
@@ -10,13 +10,16 @@ function drawSongs(songList){
   var template = "";
     for(var i = 0; i < songList.length; i++){
       template +=`<div class='row songs'>
-      <div class='col col-xs-12'><img src="${songList[i].albumArt}"></div>
-      <div id='songTitle' class='col col-xs-3'><h4> Title: ${songList[i].title}</h4></div>
-      <div class='col col-xs-3'>Album: ${songList[i].collection}</div>
+      <div class='col col-xs-12  col-lg-12'><img src="${songList[i].albumArt}"></div>
+
+
+      <div id='songTitle' class='col col-xs-4  col-lg-3'><h4> Title: ${songList[i].title}</h4></div>
       
-      <div id = 'aristName' class = "col col-xs-3">Artist: ${songList[i].artist}</div>
-      <div class='col col-xs-3'><audio controls id='audioId'><source src="${songList[i].preview}" type="audio/ogg"></audio></div>
-      <div class = 'col col-xs-6'>Price: ${songList[i].price} </div>
+      <div class='col col-xs-4 col-lg-3'><audio controls id='audioId'><source src="${songList[i].preview}" type="audio/ogg"></audio></div>
+      
+      <div id = 'aristName' class = "col col-xs-4 col-lg-3">Artist: ${songList[i].artist}</div>
+      <div class='col col-xs-6 col-lg-3'>  Album: ${songList[i].collection}            </div>
+      <div class = 'col col-xs-6 col-lg-6'>Price: ${songList[i].price} </div>
       </div>`
     }
     document.getElementById('songBox').innerHTML = template; 
